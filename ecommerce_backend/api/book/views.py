02b03e450138393367412d69985c53454f04529e
Marcin <gamer19@poczta.fm>
2020-09-22 16:05:50 +0200

    implement ordering and searching

--- ecommerce_backend/api/book/views.py
+++ ecommerce_backend/api/book/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import filters
 
 from .models import Book
 from .serializers import BookSerializer, BookGenresSerializer
@@ -13,7 +14,9 @@ class BookView():
 
 
 class BookListCreateView(BookView, generics.ListCreateAPIView):
-    pass
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author']
+    ordering_fields = ["release_date", "price"]
 
 
 class BookRetrieveUpdateDestroyView(BookView, generics.RetrieveUpdateDestroyAPIView):
