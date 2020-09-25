dac724d99813bb59b4cafa54bca6f5bdabad22eb
Marcin <gamer19@poczta.fm>
2020-09-25 16:30:56 +0200

    implement file storage

--- ecommerce_backend/api/book/models.py
+++ ecommerce_backend/api/book/models.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from django.db import models
 
 
@@ -9,13 +11,19 @@ class Book(models.Model):
         ("I", "Inne")
         ]
 
+    def generate_filename(self, filename):
+        PATH = "book_covers/"
+        ext = filename.split('.')[-1]
+        filename = f"{uuid.uuid4()}.{ext}"
+        return os.path.join(PATH, filename)
+
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=1, choices=GENRES)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     author = models.CharField(max_length=100, blank=True, null=True)
     release_date = models.DateField(blank=True, null=True)
     description = models.TextField(max_length=500, blank=True, null=True)
-    cover = models.ImageField(blank=True, null=True)
+    cover = models.ImageField(upload_to=generate_filename)
 
     class Meta:
         ordering = ['title']
