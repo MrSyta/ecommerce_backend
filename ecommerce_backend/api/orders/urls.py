ef08373d433e1515c7f2d940c6be2313967894ca
Marcin <gamer19@poczta.fm>
2020-10-05 14:37:37 +0200

    add my_cart endpoint

--- ecommerce_backend/api/orders/urls.py
+++ ecommerce_backend/api/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderListCreateView, OrderRetrieveUpdateDestroyView
+from .views import OrderListCreateView, OrderRetrieveUpdateDestroyView, MyCartView
 
 urlpatterns = [
     path('orders/', OrderListCreateView.as_view()),
     path('orders/<int:pk>/', OrderRetrieveUpdateDestroyView.as_view()),
+    path('my_cart/', MyCartView.as_view())
 ]
--- ecommerce_backend/api/orders/views.py
+++ ecommerce_backend/api/orders/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Order
 from .serializers import OrderSerializer
@@ -15,3 +19,11 @@ class OrderListCreateView(OrderView, generics.ListCreateAPIView):
 
 class OrderRetrieveUpdateDestroyView(OrderView, generics.RetrieveUpdateDestroyAPIView):
     pass
+
+
+class MyCartView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        order, created = Order.objects.get_or_create(user=request.user, complete=False)
+        return Response(OrderSerializer(order).data, status=status.HTTP_200_OK)
